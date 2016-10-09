ed0f9da23a69054c4417b3255097a40ffd7626e7
pengyiming <Yiming@Yimings-Mac.local>
2016-10-09 17:03:47 +1300

    [FEATURE] Commit problem3 for Euler Projects
    ------------------------------------------------
    Main:
    -Package: nz.ac.vuw.ecs.problems:
    
    Problem3:
    1. Create the Problem3 - "Largest prime factor".
    2. Get the correct answer.
    
    Change-Id: N/A
    Resolves: N/A
    Related: N/A
    Releases: master, 1.0.0
    Date: 9/10/2016
    Time: 5:03 PM

---
+++ src/nz/ac/vuw/ecs/problems/Problem3.java
@@ -0,0 +1,52 @@
+package nz.ac.vuw.ecs.problems;
+
+
+import java.util.ArrayList;
+
+/**
+ * This class represents the problem 3 of Project Euler
+ * "Largest prime factor"
+ * Link:
+ *      https://projecteuler.net/problem=3
+ * Status:
+ *      Correct
+ * Answer:
+ *      sum = 6857
+ *
+ * @author Yiming
+ * @version 1.0.0
+ * @date 9/10/16
+ * @since 1.0.0
+ */
+public class Problem3 {
+    private static ArrayList<Integer> list = new ArrayList<Integer>();
+
+    private static void primeFactorization(long num){
+        int i = 2;
+        long temp = num;
+        do {
+            if (temp % i == 0){
+                if (checkPrime(i)){
+                    list.add(i);
+                    temp = temp/i;
+                }
+            }
+            i++;
+        }while(i<=temp);
+    }
+
+    private static boolean checkPrime(int num){
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if ( num % i == 0){
+                count++;
+            }
+        }
+        return count <= 2;
+    }
+
+    public static void main(String[] args) {
+        primeFactorization(600851475143L);
+        System.out.println("The largest prime factor is "+ list.get(list.size() - 1));
+    }
+}
